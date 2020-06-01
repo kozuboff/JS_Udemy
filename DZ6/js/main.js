f80d63070d90242b06683f00aaff9360c048b3bd
Vladimir <vvk_mbv@mail.ru>
2020-06-01 12:50:29 +0300

    Пятое ДЗ

---
+++ DZ6/js/main.js
@@ -0,0 +1,30 @@
+let startBtn             = document.querySelector('#start'),
+    resultValue          = document.querySelectorAll('.result-table [class$="-value"]'),
+    expensesItem         = document.querySelectorAll('input.expenses-item'),
+    optionalexpensesItem = document.querySelectorAll('input.optionalexpenses-item'),
+    expensesBtn          = document.querySelector('button.expenses-item-btn'),
+    optionalexpensesBtn  = document.querySelector('button.optionalexpenses-btn'),
+    countBudgetBtn       = document.querySelector('button.count-budget-btn'),
+    optionalIncomMoney   = document.querySelector('#income'),
+    savings              = document.querySelector('#savings'),
+    sum                  = document.querySelector('#sum'),
+    percent              = document.querySelector('#percent'),
+    year                 = document.querySelector('.year-value'),
+    month                = document.querySelector('.month-value'),
+    day                  = document.querySelector('.day-value');
+
+
+console.log('startBtn= ', startBtn);
+console.log('resultValue= ', resultValue);
+console.log('expensesItem= ', expensesItem);
+console.log('optionalexpensesItem= ', optionalexpensesItem);
+console.log('expensesBtn= ', expensesBtn);
+console.log('optionalexpensesBtn= ', optionalexpensesBtn);
+console.log('countBudgetBtn= ', countBudgetBtn);
+console.log('optionalIncomMoney= ', optionalIncomMoney);
+console.log('savings= ', savings);
+console.log('sum= ', sum);
+console.log('percent= ', percent);
+console.log('year= ', year);
+console.log('month= ', month);
+console.log('day= ', day);
